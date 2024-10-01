46eb5abc8df2e55d95b09ee7f82cd2313497f6b0
Jaeeun Kim <dmjfhe12@gmail.com>
2024-10-01 15:49:57 +0100

    setting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+// 홈페이지 /
+// 영화 전체 보여주는 페이지 (서치) /movies
+// 영화 디테일 페이지 /movies/:id
+// 추천 영화 /movies/:id/recommendation
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<Homepage />} />
+        <Route path="movies">
+          <Route index element={<MoviePage />} />
+          <Route path=":id" element={<MovieDetailPage />} />
+        </Route>
+        {/* <Route path="/movies" element={<MoviePage />} />
+        <Route path="/movies/:id" element={<MovieDetailPage />} /> */}
+      </Route>
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
